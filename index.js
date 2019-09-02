0c4224f62ceadc7bd7dd59e134e061d17179798d
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-09-02 17:08:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 function deleteFromObjectByKey (object, key) {
   var clone = Object.assign({}, object);
+  delete clone[key];
   return clone;
-  delete 
 }
 function 
